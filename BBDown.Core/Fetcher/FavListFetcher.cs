af81a74630abd2c499a87617da754f54f623b1b3
Not sure <fangjinqiang125@163.com>
2025-04-29 14:45:00 +0800

    Update FavListFetcher.cs
    more diagnostics informations

--- BBDown.Core/Fetcher/FavListFetcher.cs
+++ BBDown.Core/Fetcher/FavListFetcher.cs
@@ -1,5 +1,6 @@
 ﻿using BBDown.Core.Entity;
 using System.Text.Json;
+using System.Diagnostics;
 using static BBDown.Core.Entity.Entity;
 using static BBDown.Core.Util.HTTPUtil;
 using static BBDown.Core.Logger;
@@ -13,7 +14,47 @@ namespace BBDown.Core.Fetcher;
 ///
 /// </summary>
 public class FavListFetcher : IFetcher
-{ 
+{
+    public void ExceptionWithDiagnosticsLog(String msg = "") {
+      StackFrame stackFrame = new StackTrace(new StackFrame(1,true)).GetFrame(0);
+      LogError($"Exception at {stackFrame.GetFileName()}:{stackFrame.GetFileLineNumber()}\n\tMethod {stackFrame.GetMethod().Name}, {msg}");
+    }
+
+    public JsonElement TryGetProperty(JsonElement e, String name) {
+      try {
+        return e.GetProperty(name);
+      } catch (InvalidOperationException ex) {
+        ExceptionWithDiagnosticsLog($"Could not get property {name}");
+        return null;
+      }
+    }
+    public String GetPropertyToString(JsonElement e, String name, String defaultValue = "") {
+      try {
+        return e.GetProperty(name).ToString();
+      } catch (InvalidOperationException ex) {
+        ExceptionWithDiagnosticsLog($"Could not get property {name}");
+        return defaultValue;
+      }
+    }
+
+    public Int32 GetPropertyToInt32(JsonElement e, String name, Int32 defaultValue = 0) {
+      try {
+        return e.GetProperty(name).ToInt32();
+        ExceptionWithDiagnosticsLog($"Could not get property {name}");
+      } catch (InvalidOperationException ex) {
+        return defaultValue;
+      }
+    }
+
+    public Int64 GetPropertyToInt64(JsonElement e, String name, Int64 defaultValue = 0) {
+      try {
+        return e.GetProperty(name).ToInt64();
+      } catch (InvalidOperationException ex) {
+        ExceptionWithDiagnosticsLog($"Could not get property {name}");
+        return defaultValue;
+      }
+    }
+    
     public async Task<VInfo> FetchAsync(string id)
     {
         id = id[6..];
@@ -89,32 +130,27 @@ public class FavListFetcher : IFetcher
             }
             else
             {
-                try {
-                    Page p = new(index++,
-                        m.GetProperty("id").ToString(),
-                        m.GetProperty("ugc").GetProperty("first_cid").ToString(),
-                        "", //epid
-                        m.GetProperty("title").ToString(),
-                        m.GetProperty("duration").GetInt32(),
-                        "",
-                        m.GetProperty("pubtime").GetInt64(),
-                        m.GetProperty("cover").ToString(),
-                        m.GetProperty("intro").ToString(),
-                        m.GetProperty("upper").GetProperty("name").ToString(),
-                        m.GetProperty("upper").GetProperty("mid").ToString());
-                    if (!pagesInfo.Contains(p)) pagesInfo.Add(p);
-            
-                } catch(InvalidOperationException e) {
-                    err_count++;
-                    LogError("错误发生于:pageCount>1分支");
-                    if (err_count >= 5) {
-                        LogError("错误仍然无法恢复!");
-                        throw e;
-                    } else {
-                        LogWarn("执行跳过...");
+
+                    var id_ = m.GetPropertyToString("id");
+                    if (String.IsNullOrEmpty(id_)) {
+                        LogError("致命错误，无法获取id.跳过...");
                         continue;
                     }
-                }
+                    var e_tmp_  = TryGetProperty(m, "ugc");
+                    var cid_ = GetPropertyToString(e_tmp_, "first_cid");
+                    var epid_ = ""; //epid
+                    var title_ = GetPropertyToString(m, "title");
+                    var dur_ = GetPropertyToInt32(m, "duration");
+                    var u_ = "";
+                    var pubtime_ = GetPropertyToInt64(m, "pubtime");
+                    var cover_ = GetPropertyToString(m, "cover");
+                    var intro_ = GetPropertyToString(m, "intro");
+                    e_tmp_  = TryGetProperty(m, "upper");
+                    var upper_name_ = GetPropertyToString(e_tmp_, "name");
+                    var upper_mid_ = GetPropertyToString(e_tmp_, "mid");
+                    Page p = new(index++, id_, cid_, epid_, title_, dur_, u_, pubtime_, cover_, intro_, upper_name_, upper_mid_);
+                    if (!pagesInfo.Contains(p)) pagesInfo.Add(p);
+            
             }
         }
 
